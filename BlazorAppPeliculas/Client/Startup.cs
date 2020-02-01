c85e9b30bf99dac76d615be80730708cfbef4cdb
Emilio <barrera_emilio@yahoo.es>
2020-02-01 21:19:38 +0000

    Formulario de Personas
    Componete deInsertar Imagen

--- BlazorAppPeliculas/Client/Startup.cs
+++ BlazorAppPeliculas/Client/Startup.cs
@@ -1,3 +1,4 @@
+using Blazor.FileReader;
 using BlazorAppPeliculas.Client.Repositorios;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +12,7 @@ namespace BlazorAppPeliculas.Client
             services.AddSingleton<ServiciosSingleton>();
             services.AddTransient<ServicioTransient>();
             services.AddScoped<IPeliculaRepositorio, PeliculaRepositorio>();
+            services.AddFileReaderService(options => options.InitializeOnFirstCall = true);
         }
 
         public void Configure(IComponentsApplicationBuilder app)
